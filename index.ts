66ca349d6dffe9d9c84c97a432ad1a62b7e336d6
sterlu <sterlu.sd@gmail.com>
2024-09-14 14:40:13 +0200

    Implement Xbox controller abstraction

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
 import dgram from 'node:dgram';
+import {ControllerInputEvent, ControllerState, XboxController} from "./xbox";
+import Debug from 'debug';
+
+const debug = Debug('roomba-remote:main');
+
 
 const REMOTE_IP = '192.168.50.192';
 const REMOTE_PORT = 2390;
@@ -11,3 +16,34 @@ send(128); // Start
 // send(131); // Safe mode
 // send(145, 1, 244, 1, 244) // Go
 // send(173); // Stop
+
+
+(async () => {
+    if (await XboxController.availableControllers() === 0) throw new Error('No controllers connected');
+
+    const pollHz = 10;
+    const controller = new XboxController({ controllerIndex: 0, pollHz });
+
+    controller.startPolling();
+    controller.on('input', (input: ControllerInputEvent) => {
+        if (input.type === 'button') {
+            if (input.state === 'pressed') {
+                switch (input.button) {
+                    case 'A':
+                        // Roomba.clean();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    });
+    controller.on('state', (state: ControllerState) => {
+        const { thumbLeftX, thumbLeftY, thumbRightX, thumbRightY, triggerLeft, triggerRight } = state;
+        const speedTrigger = triggerRight - triggerLeft;
+        const speed = Math.max(-500, Math.min(500, Math.round(500 * speedTrigger)));
+        const radius = Math.max(-2000, Math.min(2000, Math.round(2000 * thumbLeftX)));
+        debug({ speed, radius });
+        debug(137, speed >> 8, speed & 0xff, radius >> 8, radius & 0xff);
+    });
+})();
