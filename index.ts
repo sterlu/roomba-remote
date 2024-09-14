0a4495a4d4e6ed36bf95a939a18aa733dc1b79fe
sterlu <sterlu.sd@gmail.com>
2024-09-14 17:15:20 +0200

    Test movement logic and button presses

--- index.ts
+++ index.ts
@@ -17,6 +17,7 @@ send(128); // Start
 // send(145, 1, 244, 1, 244) // Go
 // send(173); // Stop
 
+const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 (async () => {
     if (await XboxController.availableControllers() === 0) throw new Error('No controllers connected');
@@ -24,13 +25,28 @@ send(128); // Start
     const pollHz = 10;
     const controller = new XboxController({ controllerIndex: 0, pollHz });
 
+    send(128); // Start
+    await wait(1000);
+    send(131); // Safe mode
+    await wait(1000);
+    // // send(145, 1, 244, 1, 244) // Go
+
     controller.startPolling();
     controller.on('input', (input: ControllerInputEvent) => {
+        debug(input.button, input.state);
         if (input.type === 'button') {
             if (input.state === 'pressed') {
                 switch (input.button) {
                     case 'A':
                         // Roomba.clean();
+                        send(138, 2);
+                        break;
+                    case 'START':
+                        send(128);
+                        wait(100).then(() => send(131));
+                        break;
+                    case 'BACK':
+                        send(173);
                         break;
                     default:
                         break;
@@ -38,12 +54,26 @@ send(128); // Start
             }
         }
     });
+
     controller.on('state', (state: ControllerState) => {
         const { thumbLeftX, thumbLeftY, thumbRightX, thumbRightY, triggerLeft, triggerRight } = state;
         const speedTrigger = triggerRight - triggerLeft;
-        const speed = Math.max(-500, Math.min(500, Math.round(500 * speedTrigger)));
-        const radius = Math.max(-2000, Math.min(2000, Math.round(2000 * thumbLeftX)));
-        debug({ speed, radius });
-        debug(137, speed >> 8, speed & 0xff, radius >> 8, radius & 0xff);
+        const speed = 500 * speedTrigger
+        const diff = thumbLeftX * 500 * (speed >= 0 ? 1 : -1);
+        const speedL = Math.max(-500, Math.min(500, Math.round(speed + diff)));
+        const speedR = Math.max(-500, Math.min(500, Math.round(speed - diff)));
+        // const radius = (1 / thumbLeftX) * 2000;
+        debug({ speed, diff, speedL, speedR });
+        // const command = [137, speed >> 8, speed & 0xff, radius >> 8, radius & 0xff];
+        const command = [145, speedR >> 8, speedR & 0xff, speedL >> 8, speedL & 0xff];
+        debug(command);
+        send(...command);
+
+
+        // const speedL = Math.max(-500, Math.min(500, Math.round(500 * thumbLeftY)));
+        // const speedR = Math.max(-500, Math.min(500, Math.round(500 * thumbRightY)));
+        // const command = [145, speedR >> 8, speedR & 0xff, speedL >> 8, speedL & 0xff];
+        // debug(command);
+        // send(...command);
     });
 })();
